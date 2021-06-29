6f9ed431d0b2e42a8135f06bda06d62793cb80f1
Chris Karpovich <chris.karpovich@gmail.com>
2021-06-29 16:41:38 -0400

    Started linked lists

---
+++ CTCI/chapter_02/linked_list.py
@@ -0,0 +1,43 @@
+"""
+LinkedList and LinkedListNode classes.
+"""
+
+class LinkedListNode:
+    def __init__(self, value, next_node=None, prev_node=None):
+        self.value = value
+        self.next = next_node
+        self.prev = prev_node
+
+    def __str__(self):
+        return str(self.value)
+ 
+ 
+# Linked List class contains a Node object
+class LinkedList:
+    # Function to initialize head
+    def __init__(self, values=None):
+        self.head = None
+        self.tail = None
+        if values is not None:
+            self.add_multiple(values)
+    def add_multiple(self, values):
+        for v in values:
+            self.add(v)
+    def add(self, value):
+        if self.head is None:
+            # add to head
+            self.head = LinkedListNode(value)
+            self.tail = self.head
+        else:
+            # add to tail
+            self.tail.next = LinkedListNode(value)
+            self.tail = self.tail.next
+        return self.tail
+
+    def values(self):
+        node = self.head
+        values = []
+        while node != None:
+            values.append(node.value)
+            node = node.next
+        return values
---
+++ CTCI/chapter_02/p01_remove_dups.py
@@ -0,0 +1,57 @@
+"""
+Write code to remove duplicates from an unsorted linked list.
+"""
+
+from linked_list import *
+import time
+
+
+
+def remove_dups(llist: LinkedList):
+    data = set()
+    node = llist.head
+    prev = None
+    while node != None:
+        if node.value in data:
+            # remove node since it is a duplicate
+            # check if this is the last node or not to update tail of list
+            if node.next == None:
+                llist.tail = prev
+            # update previous pointer to have correct next pointer
+            prev.next = node.next
+        else:
+            data.add(node.value)
+            prev = node
+        node = node.next
+    return llist
+testable_functions = [remove_dups]
+
+test_cases = (
+    ([], []),
+    ([1, 1, 1, 1, 1, 1], [1]),
+    ([1, 2, 3, 2], [1, 2, 3]),
+    ([1, 2, 2, 3], [1, 2, 3]),
+    ([1, 1, 2, 3], [1, 2, 3]),
+    ([1, 2, 3], [1, 2, 3]),
+)   
+
+def main():
+    
+
+    for f in testable_functions:
+        start = time.perf_counter()
+        for _ in range(100):
+            for values, expected in test_cases:
+                expected = expected.copy()
+                deduped = f(LinkedList(values))
+                assert deduped.values() == expected
+
+                deduped.add(5)
+                expected.append(5)
+                assert deduped.values() == expected
+
+        duration = time.perf_counter() - start
+        print(f"{f.__name__} {duration * 1000:.1f}ms")
+
+if __name__ == "__main__":
+    main()
